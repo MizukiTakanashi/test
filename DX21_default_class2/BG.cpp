9c09f070482f4dec0265be6ff4b1f0c33efa8c7e
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2022-11-03 21:28:50 +0900

    bbe

--- DX21_default_class2/BG.cpp
+++ DX21_default_class2/BG.cpp
@@ -18,6 +18,8 @@ const float BG::UV_SPEED = 0.005f;
 //=========================
 void BG::DrawBG()const
 {
+	int a = 0;
+
 	//テクスチャの設定
 	GetDeviceContext()->PSSetShaderResources(0, 1,
 		GetTexture(m_tex_num));
